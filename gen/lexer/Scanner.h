ff0266044aed5991f46a4127c392ccca48f4f572
Matheus Santos <vorj.dux@gmail.com>
2017-10-14 23:57:43 +0100

    Creating the first basic structure

--- gen/lexer/Scanner.h
+++ gen/lexer/Scanner.h
@@ -22,6 +22,12 @@ class Scanner: public ScannerBase
         // $insert lexFunctionDecl
         int lex();
 
+	enum Tokens
+        {
+            IDENTIFIER = 0x100,
+            NUMBER
+        };
+
     private:
         int lex__();
         int executeAction__(size_t ruleNr);
--- lexgen.sh
+++ lexgen.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env sh
 
-flexc++ src/lexer/dux.lexer --target-directory=gen/lexer/
-cp gen/lexer/Scanner*.cpp src/lexer
-cp gen/lexer/Scanner*.h include/lexer
+LEXINC_DIR='include/lexer'
+LEXSRC_DIR='src/lexer'
+LEXGEN_DIR='gen/lexer'
+
+flexc++ $LEXSRC_DIR/dux.lexer --target-directory=$LEXGEN_DIR
+
+for entry in $LEXGEN_DIR/*.cpp
+do
+	filename=$(echo $entry | sed 's/.*\/\(.*\)/\1/g')
+	echo ">>> Renegerating: $entry -> $LEXSRC_DIR/$filename"
+	echo "/**\n** This file is generated in gen/lexer.\n** If you need to change it, please change there.\n**/" > "$LEXSRC_DIR/$filename"
+	cat "$entry" | sed 's/\(\#include \"\)\(ScannerImpl.h\)\(\"\)/\1lexer\/\2\3/g' >> "$LEXSRC_DIR/$filename"
+done
+
+
+for entry in "$LEXGEN_DIR"/*.h
+do
+	filename=$(echo $entry | sed 's/.*\/\(.*\)/\1/g')
+	echo ">>> Renegerating: $entry -> $LEXINC_DIR/$filename"
+        echo "/**\n** This header file is generated in gen/lexer.\n** If you need to change it, please change there.\n**/" > "$LEXINC_DIR/$filename"
+        cat "$entry" >> "$LEXINC_DIR/$filename"
+done
